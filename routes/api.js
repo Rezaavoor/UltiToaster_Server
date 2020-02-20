e629b9ac702571875d66feb5c197b83c872bb3c7
Hosseini Reza <reza.hosseini@edu.nacka.se>
2020-02-20 14:27:39 +0100

    change the pwm library to pi-blaster.js

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { Gpio: GpioOnoff } = require("onoff");
-const { Gpio } = require("pigpio");
+const { Gpio } = require("onoff");
+const piblaster = require("pi-blaster.js");
 
-const LED = new GpioOnoff(4, "out");
+const LED = new Gpio(4, "out");
 
 router.get("/", (req, res) => {
   LED.writeSync(LED.readSync() === 1 ? 0 : 1);
@@ -15,10 +15,12 @@ router.get("/motors/:id", (req, res) => {
   //pushMotor pushes the bread to the toaster
   //liftMotor lifts down the toaster handle to start toasting
 });
-const motor = new Gpio(17, { mode: Gpio.OUTPUT });
+//const motor = new Gpio(17, { mode: Gpio.OUTPUT });
 router.get("/motor/:duration", (req, res) => {
-  const dc = (parseInt(req.params.duration) * 255) / 100;
-  motor.pwmWrite(Math.floor(dc));
+  // const dc = (parseInt(req.params.duration) * 255) / 100;
+  const dc = parseInt(req.params.duration) / 100;
+  // motor.pwmWrite(Math.floor(dc));
+  piblaster.setPwm(17, dc);
   res.send(dc + "");
 });
 router.get("/test_connection", (req, res) => {
