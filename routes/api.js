5182fdd8c03e8c9e298cf7ba3c8ca097b0197dbf
Hosseini Reza <reza.hosseini@edu.nacka.se>
2020-02-20 13:59:44 +0100

    try 1- fixing pigpio bug

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { Gpio } = require("onoff");
-const { Gpio: piGpio } = require("pigpio");
+const { GpioOnoff } = require("onoff");
+const { Gpio } = require("pigpio");
 
-const LED = new Gpio(4, "out");
+const LED = new GpioOnoff(4, "out");
 
 router.get("/", (req, res) => {
   LED.writeSync(LED.readSync() === 1 ? 0 : 1);
@@ -15,7 +15,7 @@ router.get("/motors/:id", (req, res) => {
   //pushMotor pushes the bread to the toaster
   //liftMotor lifts down the toaster handle to start toasting
 });
-const motor = new piGpio(17, { mode: piGpio.OUTPUT });
+const motor = new Gpio(17, { mode: Gpio.OUTPUT });
 router.get("/motor/:duration", (req, res) => {
   const dc = (parseInt(req.params.duration) * 255) / 100;
   motor.pwmWrite(Math.floor(dc));
