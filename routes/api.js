c1276d63d3a112d6ae4f4a2b4b32a23d23eabf18
rezaavoor <reza.avoor@gmail.com>
2020-03-11 19:17:21 +0000

    add control of timing of the motor from api

--- routes/api.js
+++ routes/api.js
@@ -2,13 +2,6 @@ const express = require("express");
 const router = express.Router();
 const Gpio = require('pigpio').Gpio;
 
-const led = new Gpio(4, { mode: Gpio.OUTPUT })
-
-router.get("/led", (req, res) => {
-	led.digitalWrite(led.digitalRead() ^ 1);
-	res.send('Led status on pin 4 changed to:' + led.digitalRead());
-});
-
 const motors = {
 	1: new Gpio(17, { mode: Gpio.OUTPUT }),
 	2: new Gpio(27, { mode: Gpio.OUTPUT }),
@@ -16,12 +9,16 @@ const motors = {
 	4: new Gpio(10, { mode: Gpio.OUTPUT }),
 	5: new Gpio(9, { mode: Gpio.OUTPUT }),
 }
-router.get("/motor/:motor/:speed", (req, res) => {
+router.get("/motor/:motor/:speed/:time", (req, res) => {
+	const time = parseInt(req.params.time) * 1000 //milliseconds
 	const motorIndex = parseInt(req.params.motor)
-	const dc = (parseInt(req.params.speed) * 10) + 1500;
+	const dc = (parseInt(req.params.speed) * 10) + 1500; //dutycycle
 	const motor = motors[motorIndex];
 	motor.servoWrite(dc);
-	res.send(`motor ${motorIndex} is set to dutycycle ${dc}`);
+	setTimeout(() => {
+		motor.digitalWrite(0)
+	}, time)
+	res.send(`motor ${motorIndex} is set to dutycycle ${dc} for ${time / 1000} seconds`);
 });
 
 const switches = {
